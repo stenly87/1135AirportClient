f914ecbe9b54bbe110cca7138d89a6d341ce9de7
ppk <ppk_student@hotmail.com>
2022-01-19 11:34:45 +1000

    Add project files.

---
+++ 1135AirportClient/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using ModelsApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Text.Json;
+namespace _1135AirportClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Test();
+        }
+
+        async Task Test()
+        {
+            var result = await Api.GetAsync<AircompanyApi>(1, "Aircompany");
+            //var city = await Api.GetListAsync<CityApi[]>("City");
+        }
+    }
+}
